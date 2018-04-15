80cef75893001dd74f3fa674cd2da7cdb9eadd59
Thanigainathan <Thanigainathan@hotmail.com>
2018-04-15 14:13:13 -0400

    Adding material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import { MatButtonModule } from "@angular/material";
 import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
